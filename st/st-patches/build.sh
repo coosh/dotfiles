b8a8ffcb9d84ef740782d496bc6ce7e6151bcc23
Robert Postill <rob.postill@nedap.com>
2019-02-05 14:16:19 -0500

    Clean up existing files

--- st/st-patches/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd ../st
-git reset --hard master
-patch < ../st-patches/st-scrollback-20181224-096b125.diff
-patch < ../st-patches/st-scrollback-mouse-0.8.diff
-patch -p1 < ../st-patches/st-visualbell2-basic-2018-10-16-30ec9a3.patch
-patch < ../st-patches/st-xresources-20190105-9f80f67.diff
-
